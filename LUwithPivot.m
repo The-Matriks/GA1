4c7b063536cb53ffbf372b25499a25bc46cb5b74
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-02 13:34:57 +0700

    LU with pivot code

---
+++ LUwithPivot.m
@@ -0,0 +1,20 @@
+function [L, U, p] = LUwithPivot(A)
+
+[m, n]  = size(A);
+p = 1:n;
+L = eye(n);
+
+for k = 1:n-1
+  [maxval, maxindex] = max(abs(A(k:n, k)));
+  maxindex = maxindex + k - 1;
+  A([k, maxindex], :) = A([maxindex, k], :);
+  L([k, maxindex], 1:k-1) = L([maxindex, k], 1:k-1);
+  p([k, maxindex]) = p([maxindex, k]);
+  for i = k+1:n 
+    L(i, k) = A(i, k) / A(k, k);
+    A(i, k:n) = A(i, k:n) - L(i, k) * A(k, k:n);
+  end
+end
+
+U = A;
+end
